35088c106a010b116e4b4a757e9e80a3f5b81852
test1414 <98690147+test1414@users.noreply.github.com>
2022-01-30 20:43:28 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__PAW_CONFIG__ = {
-    googleApiKey: 'AIzaSyBoX_m8IQZO2Fq__4XgkIRADhVXTJwtsbs',
+    googleApiKey: 'AIzaSyBM8mKe_j-Yy8qprCj3rUlpwLdTbyzShlU',
     googleSheetDocId: '13-IPT8oKyahHGO_afhz9KTeIB_S4f_Zhju1k-iC_V0g',
 }
